eb269468e04287dab12b5f58a302682136a347f7
Hazrody <116826998+Hazrody@users.noreply.github.com>
2023-10-10 15:29:32 +0200

    jsdoc

--- public/printCalendar.mjs
+++ public/printCalendar.mjs
@@ -1,10 +1,29 @@
 import createCalendar from '../node_modules/calendar/calendar.js';
 
+/**
+ * Listener pour le bouton générer calendrier
+ */
 document.addEventListener('DOMContentLoaded', () => {
+    /**
+     * Valeur mois selectionnée
+     * @type {HTMLElement}
+     */
     const monthSelect = document.getElementById('month');
+    /**
+     * Valeur année selectionné
+     * @type {HTMLElement}
+     */
     const yearInput = document.getElementById('year');
+
+    /**
+     * Le bouton
+     * @type {HTMLElement}
+     */
     const generateButton = document.getElementById('generate-calendar');
 
+    /**
+     * évènement déclanché à l'appuie du bouton
+     */
     generateButton.addEventListener('click', () => {
         const processCalendar = document.getElementById('process-calendar');
         processCalendar.innerHTML = createCalendar(monthSelect.value, yearInput.value);
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+/**
+ * Configuration du serveur node js en utilisant express
+ */
 app.use('/node_modules',express.static('node_modules'));
 app.use(express.static('public'));
-
 
 app.listen(port, () => {
     console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
